d6e4f3c23e387b7fe4e5b47d8962cbbd2fb04ee1
Eric McCormick <edm00se@users.noreply.github.com>
2024-01-02 22:21:58 +0000

    removes unused filters

--- eleventy.config.js
+++ eleventy.config.js
@@ -73,21 +73,8 @@ module.exports = async function(eleventyConfig) {
 		return Math.min.apply(null, numbers);
 	});
 
-	// Return all the tags used in a collection
-	eleventyConfig.addFilter("getAllTags", collection => {
-		let tagSet = new Set();
-		for(let item of collection) {
-			(item.data.tags || []).forEach(tag => tagSet.add(tag));
-		}
-		return Array.from(tagSet);
-	});
-
 	eleventyConfig.addFilter("filterTagList", function filterTagList(tags) {
 		return (tags || []).filter(tag => ["all", "nav", "post", "posts"].indexOf(tag) === -1);
-	});
-
-	eleventyConfig.addFilter("tagCount", function(tagCollection) {
-		return (tagCollection || []).length;
 	});
 
 	// https://stackoverflow.com/questions/69198527/how-do-you-sort-a-list-of-blog-post-tags-by-the-number-of-posts-that-contain-the/69201497#69201497
